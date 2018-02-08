8cdaff739a4b8cb6cc27016b1223c8c21a038903
Gabriel Barone <gbarone2004@gmail.com>
2018-02-08 15:30:08 -0200

    Add files via upload

---
+++ decifradecesar.py
@@ -0,0 +1,16 @@
+a = input('Texto para descriptografar: ')
+texto = []
+encoded = []
+cipher = []
+for letra in a:
+    code = ord(letra)
+    b = texto.append(code)
+    
+for num in texto:
+    encoded.append(num-1)
+    
+for x in encoded:
+    cha = chr(x)
+    y = cipher.append(cha)
+str1 = ''.join(cipher)
+print(str1)
